cfbfa1002b79ce9b6998ac4ab2ebe689e9df9fc8
FabioBis <fabio.biselli.80@gmail.com>
2015-03-28 19:48:06 +0100

    Added requirements.

--- src/sunny_as_train.py
+++ src/sunny_as_train.py
@@ -4,5 +4,23 @@
 Created on 27/mar/2015
 
 @author: Fabio Biselli
+
+sunny_as_train [OPTIONS] <SCENARIO>
+
+Options
+=======
+  --feat-range <LB, UB>
+    The features range values. The default value is [-1,1].
+  --feat-def <VALUE>
+    The feature value. The default is -1.
+  --kb-path <PATH>
+    The SCENARIO PATH, default on current folder ($PWD).
+  --kb-name <NAME>
+    The output folder name. The default value of the folder is kb_SCENARIO.
+    
+Output
+======
+  A folder called kb_NAME containing 3 files: NAME_info, NAME_lims (same as
+  current version) and NAME_args (list of definitions to be included).
 '''
 import csv
