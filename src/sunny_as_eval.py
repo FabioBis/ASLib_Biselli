353ef45a075472828b1a921d0b2f6b5422557b84
FabioBis <fabio.biselli.80@gmail.com>
2015-03-29 10:56:06 +0200

    Requirements.

---
+++ src/sunny_as_eval.py
@@ -0,0 +1,12 @@
+'''
+Created on 29/mar/2015
+
+@author: Fabio Biselli
+
+sunny_as_eval <PRED_FILES> <FEAT_FILE> <RUNS_FILE>
+
+Output
+======
+  Prints on std output a list INST_ID, PAR10 corresponding to the PAR10 score
+  of sunny-as in INST.
+'''
--- src/sunny_as_test.py
+++ src/sunny_as_test.py
@@ -4,4 +4,21 @@
 Created on 28/mar/2015
 
 @author: Fabio Biselli
+
+sunny_as_test [OPTIONS] <KB_DIR> <TEST_FILE>
+
+ptions
+=======
+  --s <SOLVER, TIME>
+    The default is not defined.
+  --k <NEIGH.SIZE>
+    The default value is sqrt(train set size).
+  --P <S1,...,Sk>
+    The default values are all the solvers.
+  --b <BACKUP>
+    (SBS).
+    
+Output
+======
+  The prediction on std output as specified in the competition rules.
 '''
